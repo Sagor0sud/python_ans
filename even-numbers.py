45ee32454eb5135fe3ceb7572301f793f7375d5f
Sagor0sud <mehedihasan74364@gmail.com>
2022-07-23 09:45:52 +0600

    odd numbers

--- even-numbers.py
+++ even-numbers.py
@@ -1,4 +1,4 @@
-# Determining periodic even numbers (use for loop)
+# Determining periodic even numbers 1 to 100 (use for loop)
 
 for x in range(1,100):
     if(x % 2 == 0):
---
+++ odd numbers.py
@@ -0,0 +1,5 @@
+# Determining periodic odd numbers 1 to 100 (use for loop)
+
+for x in range(1,100):
+    if(x % 2 == 1):
+        print(x)
